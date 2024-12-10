1c25b519c83d9a2cdc3547f8ba495870bd3b3d39
KBmahe <koushik.mahetechnologies@gmail.com>
2024-12-10 15:29:11 +0530

    Update PlayerContext.jsx

--- music-system/src/context/PlayerContext.jsx
+++ music-system/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://music-system-full-stack.onrender.com";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumData] = useState([]);
